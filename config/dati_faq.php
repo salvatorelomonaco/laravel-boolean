5e9cf3b853e687c9745aa474c78e07217385ab1d
salvatorelomonaco <lommetto95@gmail.com>
2020-02-13 15:35:31 +0100

    file con il return di un array che contiene le faq

---
+++ config/dati_faq.php
@@ -0,0 +1,46 @@
+<?php
+
+return [
+
+    'faqs_sx' => [
+
+        [
+            'q' => 'Chi può fare il corso Boolean?',
+            'a' => 'Chiunque abbia una forte curiosità verso il mondo della programmazione e voglia di imparare a fare il mestiere di sviluppatore.
+            Età e curriculum non sono una discriminante: infatti, i nostri studenti hanno dai 18 ai 45 anni, sono giovani ragazzi senza alcuna esperienza lavorativa, ma anche affermati professionisti che vogliono cambiare settore.Se non hai mai programmato o se hai già seguito qualche corso ma senti di non aver approfondito abbastanza per diventare programmatore, Boolean è il corso che fa per te.'
+        ],
+        [
+            'q' => 'Che competenze devo avere per iniziare il corso?',
+            'a' => 'Il corso parte da zero, quindi non serve alcuna competenza di programmazione, è necessario però superare il processo di selezione.'
+        ],
+        [
+            'q' => 'In cosa consiste il processo di selezione?',
+            'a' => 'Boolean è un corso intensivo che richiede impegno costante, dedizione e un’attitudine al problem solving. Per assicurarci che ogni studente abbia concrete possibilità di diventare uno sviluppatore e iniziare una nuova carriera, selezioniamo gli studenti tramite un test di logica a risposta multipla e un colloquio motivazionale. Contattaci per iniziare il tuo percorso o per saperne di più!'
+        ],
+        [
+            'q' => 'Di cosa ho bisogno per seguire il corso?',
+            'a' => 'Ti basta un PC, una webcam e una buona connessione Internet (non inferiore ai 5Mb/s).'
+        ]
+    ],
+
+    'faqs_dx' => [
+        [
+            'q' => 'Ho la certezza di essere assunto?',
+            'a' => 'Dipende tutto da te! Come dimostrano numerosi studi di settore, lo sviluppatore web è uno dei mestieri più ricercati in Italia e Boolean fornisce tutte le competenze richieste dalle aziende per iniziare a fare questo lavoro. Oltre a fornirti conoscenze di programmazione, ti presentiamo alle nostre aziende partner che assumono in tutta Italia e ti aiutiamo a prepararti al meglio per i colloqui!'
+        ],
+        [
+            'q' => 'Terminato il corso, sono supportato nella ricerca del lavoro?',
+            'a' => 'Certamente! Verrai presentato a oltre 50 aziende che operano nel settore Internet in Italia. Dalla multinazionale più quotata alla startup in ascesa, avrai l’occasione di presentare te stesso e le tue qualità da neo-sviluppatore. Inoltre, ti affianchiamo nella costruzione del CV, del portfolio di progetti e simuliamo insieme dei colloqui: il tuo successo è il nostro!'
+        ],
+        [
+            'q' => 'Quanti dei vostri ex allievi hanno ricevuto offerte di lavoro?',
+            'a' => 'Il 100%. Tutti gli studenti che hanno terminato il corso hanno ricevuto una o più offerte di lavoro. Il nostro corso fornisce solide basi di programmazione ed essendo il mestiere del developer molto ricercato, le aziende sono sempre ben disposte ad assumere persone competenti e volenterose.'
+        ],
+        [
+            'q' => 'Posso fare lo sviluppatore freelance al termine del corso?',
+            'a' => "È una possibilità, ma sconsigliamo di intraprendere questa strada subito dopo il corso: per essere sviluppatori freelance l'esperienza è fondamentale e difficilmente sono sufficienti 6 mesi. Se il tuo obiettivo è lavorare seguendo i tuoi ritmi, la strada è quella giusta: molte aziende si stanno aprendo al work from home e, con un po' di esperienza in più, siamo certi che diventerai un ottimo sviluppatore freelance!"
+        ]
+    ]
+];
+
+?>
